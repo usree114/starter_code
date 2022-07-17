f394a976adfc5dc477ca88bce9937aa85fe6fdb1
usree114 <109431818+usree114@users.noreply.github.com>
2022-07-17 09:13:04 +0530

    Task B1 completed

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// import 'login_screen.dart';
+import 'home_screen.dart';
+import 'login_screen.dart';
+// import 'edit_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MAP Exam',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const LoginScreen(),
+      home: const LoginScreen(),
+      // home: const EditScreen(),
+    );
+  }
+}
